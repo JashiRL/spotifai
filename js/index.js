ed0cd474034b37f194ebcfafed84f783c03a083d
JashiRL <j,roblesleconaqugto.mx>
2023-03-06 11:21:10 -0600

    Agregamos algunos cambios de css como ajustar los tamaños, agregamos el nombre de los artistas en las tarjetas

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const cardTop = document.querySelector('#cardTop').content
 const contenido = document.querySelector('#contenido')
 const fragment = document.createDocumentFragment()
+const btnBuscar = document.getElementById('buscador')
+
 let topTwoHundred = [] 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -31,6 +33,17 @@ const creaCards = () => {
     topTwoHundred.forEach((song) => {
         cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
         cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) => {
+            //console.log(index, size)
+            if (index === size -1) {
+                artists += item.name
+            } else {
+            artists += item.name + '/'
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
 
     
         const clone = cardTop.cloneNode(true)
@@ -39,3 +52,7 @@ const creaCards = () => {
     
         contenido.appendChild(fragment)
 }
+
+btnBuscar.addEventListener('keypress', () => {
+    console.log('tecla', btnBuscar.value)
+})
